30824294544753142726572e1b732a0054732aea
Egoluback <46494602+Egoluback@users.noreply.github.com>
2019-12-15 13:07:23 +0300

    Updated object detecting method; *it's still not done*

--- 5.2/main.py
+++ 5.2/main.py
@@ -2,66 +2,51 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+import copy
 
-def removeContrast(field:np.array):
-	print(np.amax(field))
-	m=np.amax(field)
-	field1=field.copy()
-	s=field1.shape
-	for i in range(s[0]):
-		k=0
-		for j in range(1,s[1]):
-			o=k
-			k=field1[i][j]
-			if abs(k-o)<2:
-				field1[i][j]=0
-				field1[i][j-1]=0
-			else:
-				field1[i][j]=m
-	
-	field2=field.copy()
-	s=field2.shape
-	for j in range(s[1]):
-		k=0
-		for i in range(1,s[0]):
-			o=k
-			k=field2[i][j]
-			if abs(k-o)<2:
-				field2[i][j]=0
-				field2[i-1][j]=0
-			else:
-				field2[i][j]=m
-	
-	for j in range(s[1]):
-		for i in range(1,s[0]):
-			if field1[i][j]==m or field2[i][j]==m:
-				field[i][j]=m
-			else: field[i][j]=0
-	
+from pyimagesearch.shapedetector import ShapeDetector
+
+THRESHOLD = 50
+
+def removeContrast(inputField:np.array, r:int, c:int):
+	field = copy.deepcopy(inputField[r * 200:(r + 1) * 200, c * 200:(c + 1) * 200])
+
 	return field
 
 def get_winner(field:np.array):
-    field = removeContrast(field)
+	cv2.imshow("Image", field / np.amax(field))
 	
-    field /= np.amax(field)
-    image = field
+	maxValue = np.amax(field)
 	
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
+	field /= np.amax(maxValue)
 
-    gray = cv2.cvtColor(field, cv2.COLOR_BGR2GRAY)
+	for i in range(10):
+		for j in range(10):
+			print(i, j)
+			piece = removeContrast(field, i, j)
 
-    thresh = cv2.threshold(gray, 0.2, 255, cv2.THRESH_BINARY)[1]
+			piece *= 255
+			piece = piece.astype(np.uint8)
 
-    print(thresh)
-    
-    cv2.imshow("Image", thresh)
-    cv2.waitKey(0)
+			thresh = cv2.threshold(piece, THRESHOLD, 255, cv2.THRESH_BINARY)[1]
 
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	# cnts = imutils.grab_contours(cnts)
+			cv2.imshow("Image", thresh)
+			cv2.waitKey(0)
 
-    return field.shape
+			cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+			cnts = imutils.grab_contours(cnts)
+
+			shapeDetector = ShapeDetector(0.1)
+
+			if (len(cnts) > 0):
+
+				result = shapeDetector.detect(cnts[0])
+
+				cv2.putText(thresh, str(result), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+				cv2.imshow("Image", thresh)
+				cv2.waitKey()
+
+	return result
 
 
 if __name__ == "__main__":
---
+++ 5.2/pyimagesearch/shapedetector.py
@@ -0,0 +1,24 @@
+import cv2
+import math
+
+class ShapeDetector:
+    def __init__(self, accuracy):
+        self.accuracy = accuracy
+        
+    def detect(self, c):
+        shape = 'unidentified'
+        p = cv2.arcLength(c, True)
+        print('p=',p)
+        a = cv2.approxPolyDP(c, self.accuracy * p, True)
+        print(len(a),a)
+        if (len(a)==3):
+            shape = '2'
+        elif (len(a) == 4):
+            (x,y,w,h) = cv2.boundingRect(a)
+            ar = w/h
+            shape = '0'
+            if (abs(ar-1)>0.05):
+                shape = 'r'
+        else:
+            shape = '1'
+        return shape
